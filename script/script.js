0bc8477e8a01dbd3fcc644bf6a95064281602912
dkouznetsova <91774385+dkouznetsova@users.noreply.github.com>
2022-05-23 02:28:25 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+$('.bxslider').bxSlider({
+pager: true,
+});
+});
+
+$(document).ready(function(){
+$('.bxslider_team').bxSlider({
+pager: true,
+});
+});
+
+$(document).ready(function(){
+$('.flexslider').flexslider({
+animation: "slide",
+animationLoop:  false,
+itemWidth: 200,
+itemMargin: 25,
+touch: true,
+});
+});
